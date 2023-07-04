37af7aa2364043aabdbb8389202a96425a1ee1d7
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-04 12:55:35 +0300

    Build auth card.

---
+++ src/components/auth/common/AuthCard.tsx
@@ -0,0 +1,46 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+interface AuthCardProps {
+  children: React.ReactNode;
+  heading: string;
+  text: string;
+}
+
+const AuthCard = ({ children, heading, text }: AuthCardProps) => {
+  return (
+    <Flex
+      minH='100vh'
+      align='center'
+      justify='center'
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
+      <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
+        <Stack align='center'>
+          <Heading fontSize='4xl' textAlign='center'>
+            {heading}
+          </Heading>
+          <Text fontSize='lg' color='gray.600'>
+            {text}
+          </Text>
+        </Stack>
+        <Box
+          rounded='lg'
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow='lg'
+          p={8}
+        >
+          <Stack spacing={4}>{children}</Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default AuthCard;
