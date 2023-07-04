8b878bd1fa67009c9365053e42328d27d5bbb3e5
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-05 00:58:39 +0300

    Implement logo.

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { Flex } from '@chakra-ui/react';
+
+import { FcTodoList } from 'react-icons/fc';
+
+const Logo = () => {
+  return (
+    <Flex flex={1} alignItems='center' justifyContent='flex-start'>
+      <FcTodoList size={30} color='red' />
+    </Flex>
+  );
+};
+
+export default Logo;
