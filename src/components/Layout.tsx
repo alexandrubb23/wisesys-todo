bed2c5d7f45da81800b35c9fdf56fe627ff8d014
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-05 14:33:07 +0300

    Add layout color mode.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,11 @@
-import { Box } from '@chakra-ui/react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
 import NavBar from './NavBar';
 
 const Layout = () => {
   return (
-    <Box padding={5}>
+    <Box padding={5} bg={useColorModeValue('gray.50', 'gray.800')}>
       <NavBar />
       <Outlet />
     </Box>
--- src/components/auth/common/AuthCard.tsx
+++ src/components/auth/common/AuthCard.tsx
@@ -15,12 +15,7 @@ interface AuthCardProps {
 
 const AuthCard = ({ children, heading, text }: AuthCardProps) => {
   return (
-    <Flex
-      minH='100vh'
-      align='center'
-      justify='center'
-      bg={useColorModeValue('gray.50', 'gray.800')}
-    >
+    <Flex minH='100vh' align='center' justify='center'>
       <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
         <Stack align='center'>
           <Heading fontSize='4xl' textAlign='center'>
