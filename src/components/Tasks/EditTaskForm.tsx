7a6ad65cdfab2d1b13f818f1d66977fb86353c63
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-05 10:14:32 +0300

    Implement validation on edit task.

--- src/components/Tasks/EditTaskForm.tsx
+++ src/components/Tasks/EditTaskForm.tsx
@@ -1,75 +1,68 @@
-import {
-  Button,
-  FormLabel,
-  HStack,
-  Input,
-  Textarea,
-  VStack,
-} from '@chakra-ui/react';
-import { useState } from 'react';
+import { Button, HStack, VStack } from '@chakra-ui/react';
+import * as Yup from 'yup';
 
-import { useTaskDrawerContext } from './hooks';
+import { Form } from '@/components/common/Form/common';
+import { InputTypes } from '@/components/common/Form';
 import { Task } from '@/components/common/models';
 import { useMutateTask } from '@/hooks/mutation';
+import { useTaskDrawerContext } from './hooks';
 
 interface EditTaskFormProps {
   task: Task;
 }
 
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(3).max(75).label('Title'),
+  description: Yup.string().required().min(3).label('Description'),
+});
+
+type FormData = Yup.InferType<typeof validationSchema>;
+
 const EditTaskForm = ({ task }: EditTaskFormProps) => {
+  const initialValues: FormData = {
+    title: task.title,
+    description: task.description,
+  };
+
   const { editTask } = useMutateTask();
 
   const { firstField, onClose } = useTaskDrawerContext();
 
-  const [editedTask, setEditedTask] = useState<Task>({
-    id: task.id,
-    title: task.title,
-    createDate: task.createDate,
-    description: task.description,
-  });
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  const handleSubmit = async (
+    editedTask: Pick<Task, 'title' | 'description'>
   ) => {
-    setEditedTask({ ...editedTask, [e.target.name]: e.target.value });
-  };
-
-  const handleFormSubmit = () => {
-    editTask.mutate(editedTask);
+    editTask.mutate({
+      ...editedTask,
+      id: task.id,
+      createDate: task.createDate,
+    });
   };
 
   return (
     <>
-      <VStack align='left' spacing={4} alignContent='left'>
-        <FormLabel alignContent='left'>Title</FormLabel>
-        <Input
-          ref={firstField}
-          placeholder='Task title...'
-          value={editedTask.title}
-          onChange={handleChange}
-          name='title'
-        />
-
-        <FormLabel>Description</FormLabel>
-        <Textarea
-          placeholder='Task description...'
-          value={editedTask.description}
-          onChange={handleChange}
-          name='description'
-        />
-      </VStack>
-      <HStack mt={4} spacing={2}>
-        <Button variant='outline' onClick={onClose}>
-          Cancel
-        </Button>
-        <Button
-          colorScheme='blue'
-          isLoading={editTask.isLoading}
-          onClick={handleFormSubmit}
-        >
-          Save
-        </Button>
-      </HStack>
+      <Form
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        formOptions={{
+          disableButtonWhenFormIsInvalid: true,
+        }}
+      >
+        <VStack align='left' spacing={4} alignContent='left'>
+          <InputTypes.Text name='title' label='Title' ref={firstField} />
+          <InputTypes.TextArea name='description' label='Description' />
+          <HStack mt={4} spacing={2}>
+            <Button variant='outline' onClick={onClose}>
+              Cancel
+            </Button>
+            <InputTypes.SubmitButton
+              isLoading={editTask.isLoading}
+              title='Save'
+              colorScheme='blue'
+            />
+          </HStack>
+        </VStack>
+      </Form>
     </>
   );
 };
