8e31ccf395f2440fcf9482de7a09d93f880ebe95
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-06 23:20:28 +0300

    Build TableColumnSortIcon component.

---
+++ src/components/common/Table/TableColumnSortIcon.tsx
@@ -0,0 +1,20 @@
+import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
+
+import { Column, SortColumn } from '../models';
+import { TableData } from './Table';
+
+interface SortColumnIconProps<TFields extends TableData<TFields>> {
+  column: Column<TFields>;
+  sortColumn: SortColumn;
+}
+
+const TableColumnSortIcon = <TFields extends TableData<TFields>>({
+  column,
+  sortColumn,
+}: SortColumnIconProps<TFields>) => {
+  if (column.path !== sortColumn.path) return null;
+
+  return sortColumn.order === 'asc' ? <ChevronUpIcon /> : <ChevronDownIcon />;
+};
+
+export default TableColumnSortIcon;
