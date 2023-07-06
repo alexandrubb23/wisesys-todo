f2deb123959f344aebfc98c56ae13d36e7e13452
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2023-07-06 15:55:25 +0300

    Extract Logo to layout.

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -2,7 +2,7 @@ import { Flex } from '@chakra-ui/react';
 
 import { UserAccount } from '@/components/User';
 import ColorModeSwitch from './ColorModeSwitch';
-import Logo from '../Logo';
+import Logo from './Logo';
 
 const NavBar = () => {
   return (
--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,3 +1,4 @@
 export { default as ColorModeSwitch } from './ColorModeSwitch';
 export { default as Layout } from './Layout';
+export { default as Logo } from './Logo';
 export { default as NavBar } from './NavBar';
